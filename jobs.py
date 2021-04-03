e488648ffa89a3c8e00b8ede503b375e1005f503
2blackdragon <73791808+2blackdragon@users.noreply.github.com>
2021-04-03 18:38:21 +0300

    Add files via upload
    Форма регистрации

---
+++ jobs.py
@@ -0,0 +1,21 @@
+import sqlalchemy
+from data.db_session import SqlAlchemyBase
+import sqlalchemy.orm as orm
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=True, default=0)
+    collaborators = sqlalchemy.Column(sqlalchemy.String)
+    start_date = sqlalchemy.Column(sqlalchemy.DateTime)
+    end_date = sqlalchemy.Column(sqlalchemy.DateTime)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+
+    team_leader = sqlalchemy.Column(sqlalchemy.String, sqlalchemy.ForeignKey("users.id"))
+    user = orm.relation('User')
+
+    def __repr__(self):
+        return f"<Job> {self.job}"
